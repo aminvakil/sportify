88f092a7089c32eee08253c0838b8e42dcce891a
Amin Vakil <info@aminvakil.com>
2026-05-24 13:02:04 +0330

    Simplify production database initialization

--- docker/prod/init.sh
+++ docker/prod/init.sh
@@ -10,19 +10,7 @@ run_console() {
     $CONSOLE "$@"
 }
 
-wait_for_database() {
-    while true; do
-        if $CONSOLE doctrine:database:create --if-not-exists --no-interaction >/tmp/sportify-prod-db-wait.log 2>&1; then
-            rm -f /tmp/sportify-prod-db-wait.log
-            return 0
-        fi
-
-        echo "Waiting for database..." >&2
-        sleep 2
-    done
-}
-
-wait_for_database
+run_console doctrine:database:create --if-not-exists --no-interaction
 run_console doctrine:schema:update --force --no-interaction
 run_console assets:install web --symlink --relative
 run_console cache:clear --no-warmup
