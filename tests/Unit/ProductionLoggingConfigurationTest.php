373537a25aa6c7452975199212daebfaf5c11363
Amin Vakil <info@aminvakil.com>
2026-07-10 21:11:05 +0330

    test: cover application runtime warning events

--- tests/Unit/ProductionLoggingConfigurationTest.php
+++ tests/Unit/ProductionLoggingConfigurationTest.php
@@ -7,6 +7,20 @@ use Symfony\Component\Yaml\Yaml;
 
 class ProductionLoggingConfigurationTest extends TestCase
 {
+    public function testOperationalWarningsBypassErrorTriggeredHandler()
+    {
+        $projectDir = dirname(__DIR__, 2);
+        $baseConfig = Yaml::parseFile($projectDir.'/config/packages/monolog.yml');
+        $prodConfig = Yaml::parseFile($projectDir.'/config/packages/prod/monolog.yml');
+        $handlers = $prodConfig['monolog']['handlers'];
+
+        $this->assertContains('operations', $baseConfig['monolog']['channels']);
+        $this->assertSame('stream', $handlers['operational_warnings']['type']);
+        $this->assertSame('warning', $handlers['operational_warnings']['level']);
+        $this->assertSame(array('operations'), $handlers['operational_warnings']['channels']);
+        $this->assertContains('!operations', $handlers['main']['channels']);
+    }
+
     public function testPredictionWarningsBypassErrorTriggeredHandler()
     {
         $projectDir = dirname(__DIR__, 2);
